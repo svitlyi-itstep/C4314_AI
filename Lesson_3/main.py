d7844e8f079f6759797b4c9cfde9d14c4d9eb830
svitlyi <svitlyi_a@itstep.academy>
2026-05-16 15:45:40 +0300

    Lesson 2 code

---
+++ Lesson_3/main.py
@@ -0,0 +1,28 @@
+from google import genai # pip install google-genai
+from rules import rules_list
+
+
+client = genai.Client()
+
+print(" - Асистента запущено! -")
+
+while True:
+    user_prompt = input("\n> ")
+
+    if user_prompt in ["exit", "quit", "stop"]:
+        break
+
+    prompt = (f"Виконай запит користувача: {user_prompt}\n"
+              "Обов'язково виконуй наступні правила:\n"
+              "\n".join(rules_list))
+
+    # Доступні моделі:
+    # gemini-3-flash-preview
+    # gemini-2.5-flash
+
+    response = client.models.generate_content(
+        model="gemini-3-flash-preview",
+        contents=prompt,
+    )
+
+    print("\n", response.text)
