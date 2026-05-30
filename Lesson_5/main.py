903c25fc64e3d64d0a28450fc556cd3b256d4a7d
svitlyi <svitlyi_a@itstep.academy>
2026-05-30 17:35:21 +0300

    Lesson 5 code

--- Lesson_5/main.py
+++ Lesson_5/main.py
@@ -2,14 +2,30 @@ import tkinter as tk
 from tkinter import scrolledtext
 from rules import rules_list
 from google import genai # pip install google-genai
+from PIL import Image, ImageTk # pip install pillow
+import json
 
-client = genai.Client()
+client = genai.Client() # api_key="<KEY>"
+emotions = {}
 
 def print_message(message, tag="response"):
     chat_scrolled.configure(state="normal")
     chat_scrolled.insert("end", message + "\n\n", tag)
     chat_scrolled.configure(state="disabled")
     chat_scrolled.see("end")
+
+def load_emotions():
+    global emotions
+    # Normal emotion
+    img = Image.open("img/normal.png").resize((100, 80))
+    emotions["normal"] = ImageTk.PhotoImage(img)
+
+    # Thinking emotion
+    img = Image.open("img/thinking.png").resize((100, 100))
+    emotions["thinking"] = ImageTk.PhotoImage(img)
+
+def change_emotion(emotion):
+    emotion_label.config(image=emotions[emotion])
 
 def onclick(event=None):
     user_prompt = prompt_entry.get()
@@ -21,14 +37,20 @@ def onclick(event=None):
 
         prompt = (f"Виконай запит користувача: {user_prompt}\n"
                   "Обов'язково виконуй наступні правила:\n"
-                  f"{'\n'.join(rules_list)}")
+                  f"{'\n'.join(rules_list)}\n"
+                  f"Доступні емоції: {emotions.keys()}")
 
         response = client.models.generate_content(
             model="gemini-2.5-flash",
             contents=prompt,
         )
+        response_json = json.loads(str(response.text) \
+                         .removeprefix("```json") \
+                         .removesuffix("```"))
+        answer, emotion = response_json["answer"], response_json["emotion"]
         # response_label.config(text=str(response.text))
-        print_message(str(response.text), "response")
+        print_message(answer, "response")
+        change_emotion(emotion)
     except Exception as e:
         print(e)
 
@@ -36,10 +58,12 @@ window = tk.Tk()
 window.title("Візуальний асистент")
 window.geometry("300x400")
 
-response_label = tk.Label(text="Response")
-response_label.pack()
+load_emotions()
+emotion_label = tk.Label()
+emotion_label.pack()
+change_emotion("normal")
 
-chat_scrolled = scrolledtext.ScrolledText(height=20, state="disabled",
+chat_scrolled = scrolledtext.ScrolledText(height=15, state="disabled",
                 wrap="word")
 chat_scrolled.pack(fill="both", expand=True)
 
